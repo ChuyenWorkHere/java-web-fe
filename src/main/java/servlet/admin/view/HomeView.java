cbc4e333362ab2755a3e5317c9f1c01cb9e0de48
zanchuyn <vanchuyenle2004@gmail.com>
2025-05-16 18:02:42 +0700

    fix ui

--- src/main/java/servlet/admin/view/HomeView.java
+++ src/main/java/servlet/admin/view/HomeView.java
@@ -26,13 +26,6 @@ public class HomeView extends HttpServlet {
 		
 		PrintWriter out = response.getWriter();
 		
-		HttpSession session = request.getSession(true);
-		Object isLoggedIn = session.getAttribute("userLogined");
-		
-		if( isLoggedIn == null || !isLoggedIn.equals("ok")) {
-			response.sendRedirect("/Furniture/admin/signin");
-		}
-		
 		request.setAttribute("view", "home");
 		RequestDispatcher headerDispatcher = request.getRequestDispatcher("/admin/header-view");
 	    headerDispatcher.include(request, response);
