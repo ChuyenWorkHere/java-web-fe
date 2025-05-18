9d40d63756d1f1feba3a06e1ed05a26535eee5cb
Nguyen The Anh <nguyenanhhk111@gmail.com>
2025-05-18 21:10:18 +0700

    feature

--- src/main/java/servlet/models/User.java
+++ src/main/java/servlet/models/User.java
@@ -1,5 +1,17 @@
 package servlet.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    private int userId;
+    private String username;
+    private String email;
+    private String password;
+
 
 }
